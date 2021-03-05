663253ef65ecd7b1f75c5894d4f4bb709d50d5b1
Necralma <lihuel.almada@gmail.com>
2021-03-05 09:16:28 -0300

    add buttons

---
+++ components/Panel.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Dimensions, Button, View } from 'react-native';
+export default () => {
+    return(
+      <View style={styles.panel}>
+          <Button title="Lista"></Button>
+          <Button title="Mostrar/Ocultar"></Button>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  panel: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center'
+  }
+})
