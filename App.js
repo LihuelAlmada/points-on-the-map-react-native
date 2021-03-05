af1525e758e7e36b905c6196b417d2fa90708e27
Necralma <lihuel.almada@gmail.com>
2021-03-05 16:03:07 -0300

    add points

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import { Map, Modal, Panel } from './components';
 
 export default function App() {
+  const [points, setPoints] = useState([])
+  const handleLongPress = ({ nativeEvent }) => {
+    const newPoint = points.concat({ coordinate: nativeEvent.coordinate })
+    setPoints(newPoint)
+  }
+  console.log(points)
   return (
     <View style={styles.container}>
-      <Map />
+      <Map onLongPress={handleLongPress}/>
       <Panel />
       <Modal />
       <StatusBar style="auto" />
